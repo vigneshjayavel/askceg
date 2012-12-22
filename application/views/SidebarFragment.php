3c947147b25e5a5db48151d52ccc99b33f5cd907
vishnu <vishnu@vishnu-Notebook.(none)>
2012-12-22 22:53:12 +0545

    sidebar modified

--- application/views/SidebarFragment.php
+++ application/views/SidebarFragment.php
@@ -8,9 +8,7 @@
               <li><a href="<?php echo base_url() ?>QuestionsController/AskQuestion">AskQuestion</a></li>
               <li><a href="<?php echo base_url() ?>QuestionsController/AnswerQuestion">AnswerQuestion</a></li>
               <li><a href="<?php echo base_url() ?>QuestionsController/CreateDiscussion">CreateDiscussion</a></li>
-              <li class="nav-header">Search</li>
-              <li><a href="<?php echo base_url() ?>SearchController/SearchDiscussion">SearchDiscussion</a></li>
-              <li><a href="<?php echo base_url() ?>SearchController/SearchQuestion">SearchQuestion</a></li>
+             
               <li class="nav-header">Category</li>
               <li><a href="<?php echo base_url() ?>QuestionsController/ViewQuestion/1">Education</a></li>
               <li><a href="<?php echo base_url() ?>QuestionsController/ViewQuestion/2">Entertainment</a></li>
