ad19a19f820feee9be4a8310801c765975e6506d
vikki <dgr8geek@gmail.com>
2013-03-31 20:41:36 +0530

    added jq min lib

--- application/views/Skeleton.php
+++ application/views/Skeleton.php
@@ -58,7 +58,16 @@
     <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo base_url()?>assets/ico/apple-touch-icon-114-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo base_url()?>assets/ico/apple-touch-icon-72-precomposed.png">
     <link rel="apple-touch-icon-precomposed" href="<?php echo base_url()?>assets/ico/apple-touch-icon-57-precomposed.png">
+    <?php 
+    if(ENVIRONMENT=="local"){
+    ?>
     <script src="<?php echo base_url()?>assets/js/jquery.js"></script>
+    <?php
+    }
+    else{?>
+    <script src="<?php echo base_url()?>assets/js/jquery.min.js"></script>
+    <?php
+    }?>
     <script src="<?php echo base_url()?>assets/js/bootstrap.js"></script>
 
     <script src="<?php echo base_url()?>assets/js/myscripts/myReusableGlobalScript1.js"></script>
