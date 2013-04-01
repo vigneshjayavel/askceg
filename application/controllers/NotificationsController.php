614974bed10d6672366ebef5887a4cec600474e0
vikki <dgr8geek@gmail.com>
2013-04-01 12:01:25 +0530

    adding untracked file

---
+++ application/controllers/NotificationsController.php
@@ -0,0 +1,31 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class NotificationsController extends CI_Controller {
+
+	public function getNewNotificationsCount($user_id)
+	{	
+		$this->load->model('notificationsmodel');
+		$notifObj['count']=	$this->notificationsmodel->sqlFetchNotificationCount($user_id);
+		echo json_encode($notifObj);
+	}
+
+
+	public function getNewNotifications()
+	{	
+
+		$this->load->model('notificationsmodel');
+		
+		$userId=$this->session->userdata('user_id');
+			$content=$this->notificationsmodel->sqlFetchNotifications($userId,'unread');
+		$data['centerContent']='<h4>Your recent notifications</h4>'.$content;
+		
+		$this->load->view('Skeleton',$data);
+	}
+
+	public function updateNotificationReadStatus($notif_id){
+		$user_id=$this->session->userdata('user_id');
+		$this->load->model('notificationsmodel');
+		$this->notificationsmodel->sqlUpdateNotificationStatus($notif_id,$user_id);
+		echo json_encode(array('status'=>1));
+	}
+}
